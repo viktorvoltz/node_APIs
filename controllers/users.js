71f132799a4f1f77594eb55e8053d5f10faf7cf9
viktorvoltz <vctrchinyeaka@gmail.com>
2021-12-03 12:07:49 +0100

    routes

---
+++ controllers/users.js
@@ -0,0 +1,65 @@
+import dbUtil from '../dbUtil.js';
+import { v4 as uuidv4 } from 'uuid';
+
+let users = [
+    
+]
+
+
+export const createUsers = async (req, res) => {
+    const user = req.body;
+
+    const userId = uuidv4();
+    const userWithId = { ...user, id: userId}
+    users.push(userWithId);
+
+    res.send(`user with the username ${user.F_name} added to the database!`);
+    try {
+        await dbUtil.createuser(user)
+        res.status(200).send({ message: "user added to Mongod database", data: null })
+      } catch (error) {
+        res.status(400).send({ message: "could add user to Mongod" })
+      }
+
+}
+
+export const getUsers = (req, res) => {
+    res.send(users);
+    res.send('hello');
+}
+
+export const getUser = (req, res) => {
+    const { id } = req.params;
+    //const id = req.params.id;
+
+    const foundUser = users.find((user) => {
+        return user.id === id;
+    })
+
+    res.send(foundUser);
+}
+
+export const deleteUser = (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id);
+    
+    res.send(`user with the id: ${id} deleted.`);
+}
+
+export const updateUser = (req, res) => {
+    const {id} = req.params;
+    const {F_name, L_name, age} = req.body;
+    
+    
+    const user = users.find((user) => user.id === id);
+    
+    if(F_name) user.F_name = F_name;
+
+    if(L_name) user.L_name = L_name;
+    if(age) user.age = age;
+
+    res.send(`User with id: ${id} has been updated`);
+    
+    
+}
--- routes/users.js
+++ routes/users.js
@@ -1,45 +1,21 @@
 import express from 'express';
-import dbUtil from '../dbUtil.js';
-import { v4 as uuidv4 } from 'uuid';
+import { createUsers, getUsers, getUser, deleteUser, updateUser  } from '../controllers/users.js';
 
 const router = express.Router();
 
-const users = [
-    
-]
+
 
 //routes begin with /users
-router.get('/', (req, res) => {
-    res.send(users);
-    res.send('hello');
-});
+router.get('/', getUsers);
 
-router.post('/', async (req, res) => {
-    const user = req.body;
 
-    const userId = uuidv4();
-    const userWithId = { ...user, id: userId}
-    users.push(userWithId);
 
-    res.send(`user with the username ${user.F_name} added to the database!`);
-    try {
-        await dbUtil.createuser(user)
-        res.status(200).send({ message: "user added to Mongod database", data: null })
-      } catch (error) {
-        res.status(400).send({ message: "could add user to Mongod" })
-      }
+router.post('/', createUsers);
 
-})
+router.get('/:id', getUser);
 
-router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    //const id = req.params.id;
+router.delete('/:id', deleteUser);
 
-    const foundUser = users.find((user) => {
-        return user.id === id;
-    })
-
-    res.send(foundUser);
-})
+router.patch('/:id', updateUser)
 
 export default router;
